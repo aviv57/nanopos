a219dcbb46a68b073989846fa3164ad79a3da2a6
me <yosef1x@barzel.com>
2019-12-18 15:09:27 +0200

    Add NODE_ENV="development" and load config.env in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 [ -f .env ] && source .env
+[ -f config.env ] && source config.env
+export NODE_ENV="development"
 babel-watch --extensions .js,.pug,.yaml --watch src --watch views --watch . --exclude node_modules src/cli.js -- "$@"
